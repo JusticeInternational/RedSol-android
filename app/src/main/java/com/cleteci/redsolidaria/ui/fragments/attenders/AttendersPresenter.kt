3e21669e9bf4757c52acac083bff27e90d961ac6
Betania Beltrán <beltranbetania@gmail.com>
2020-10-23 16:06:56 -0400

    new Frag

---
+++ app/src/main/java/com/cleteci/redsolidaria/ui/fragments/attenders/AttendersPresenter.kt
@@ -0,0 +1,27 @@
+package com.cleteci.redsolidaria.ui.fragments.users
+
+import io.reactivex.disposables.CompositeDisposable
+
+/**
+ * Created by ogulcan on 07/02/2018.
+ */
+class UsersPresenter: UsersContract.Presenter {
+
+    private val subscriptions = CompositeDisposable()
+    private lateinit var view: UsersContract.View
+
+    override fun subscribe() {
+
+    }
+
+    override fun unsubscribe() {
+        subscriptions.clear()
+    }
+
+    override fun attach(view: UsersContract.View) {
+        this.view = view
+        view.init() // as default
+    }
+
+
+}
