fd5af90313925f263dc9f819fcff3db24b45d074
Alexandra Perdomo <aperdomo@mahisoft.com>
2021-01-25 18:20:57 -0600

    Adding google map api key new configuration

--- app/src/main/java/com/cleteci/redsolidaria/ui/fragments/myProfileProvider/MyProfileProviderFragment.kt
+++ app/src/main/java/com/cleteci/redsolidaria/ui/fragments/myProfileProvider/MyProfileProviderFragment.kt
@@ -111,7 +111,7 @@ class MyProfileProviderFragment : BaseFragment(), MyProfileProviderContract.View
         etLocation!!.setOnClickListener {
             val locationPickerIntent = LocationPickerActivity.Builder()
                 .withLocation(41.4036299, 2.1743558)
-                .withGeolocApiKey(getString(R.string.map_key))
+                .withGeolocApiKey(getString(R.string.google_maps_key))
                 .withSearchZone("es_ES")
 
                 .build(context!!)
